3c91ae9ee6a9ca812a4af56009ab66fe636bd8a4
Lucas Burns <lucas@burns.io>
2017-12-09 19:13:35 -0800

    Updated responses and fixed 0 or negative player checks

--- bot.js
+++ bot.js
@@ -4,58 +4,73 @@ const prefix = "!";
 const CHECK_READY_CMD = "cready";
 const CHECK_NUM_CMD = "?";
 const READY_CMD = "ready";
+const USAGE = prefix + CHECK_READY_CMD;
 var check;
 var ready = [];
 var checker = "";
 
-client.on("ready", () => {
-	console.log("I am ready");
-});
-
 client.on("message", (m) => {
-	if (m.content.startsWith(prefix)) {
-		const args = m.content.slice(prefix.length).trim().split(/ +/g);
+	var cont = m.content;
+	var chan = m.channel;
+	var auth = m.author;
+	if (cont.startsWith(prefix)) {
+		const args = cont.slice(prefix.length).trim().split(/ +/g);
 		const command = args.shift().toLowerCase();
 		if (command == CHECK_READY_CMD) {
 			if (args[0] == CHECK_NUM_CMD) {
 				if (check) {
-					m.channel.send("Still waiting for " + (check - ready.length) + " players to ready.");
-				}
-			}
-			else if (isNaN(args[0])) {
-				if (typeof args[0] == 'undefined') {
-					m.channel.send("How many players do you want to wait for? Type " + prefix + CHECK_READY_CMD + " <number> to wait for your desired number of players to ready!");
+					var numLeft = (check - ready.length);
+					chan.send("Still waiting for " + numLeft + " player" + (numLeft != 1 ? "s" : "") + " to ready.");
 				}
 				else {
-					m.channel.send("What? You can't have \"" + args[0] + "\" players to check...");
+					noRCAck(chan, "No ready check active.");
+				}
+			}
+			else if (isNaN(args[0]) || args[0] <= 0) {
+				if (typeof args[0] == 'undefined') {
+					noRCAck(chan, "How many players do you want to wait for?");
+				}
+				else {
+					noRCAck(chan, "What? You can't have \"" + args[0] + "\" player" + (check != 1 ? "s" : "") + " to check.");
 				}
 			}
 			else {
 				check = args[0];
-				checker = m.author;
-				m.channel.send("Ready up! Waiting for " + check + " players.");
+				checker = auth;
+				var numLeft = (check - ready.length);
+				chan.send("@everyone ready up! Waiting for " + check + " player" + (numLeft != 1 ? "s" : "") + ".");
 			}
 		}
 		if (command == READY_CMD) {
 			if (check) {
-				if (ready.indexOf(m.author) == -1) {
-					ready.push(m.author);
-					m.channel.send(m.author + " is ready! " + (check - ready.length) + " players left.");
+				if (ready.indexOf(auth) == -1) {
+					ready.push(auth);
+					var numLeft = (check - ready.length);
+					chan.send(auth + " is ready! " + numLeft + " player" + (numLeft != 1 ? "s" : "") + " left.");
 					if (check == ready.length) {
 						check = false;
 						ready = [];
-						m.channel.send("Ready check complete, " + checker + ". Lets go!");
+						chan.send("Ready check complete, " + checker + ". Lets go!");
 					}
 				}
 				else {
-					m.channel.send("You've already readied!");
+					chan.send("You've already readied!");
 				}
 			}
 			else {
-				m.channel.send("No ready check active. Type " + prefix + CHECK_READY_CMD + " to start a ready check");
+				noRCAck(chan, "No ready check active.");
 			}
+		}
+		if (command == "help") {
+			chan
 		}
 	}
 });
+
+function noRCAck (chan, reason) {
+	chan.send((typeof reason == 'undefined' ? "" : reason + " ") + "Type ```" + USAGE + 
+	" <number>``` to start a ready check, and ```" + USAGE + " " + CHECK_NUM_CMD + 
+	"``` to check how many players still need to ready.");
+}
 
 client.login("Mzg5MjEwNjQwNjEyNTg5NTY4.DQ4TPw.vlSsoyWy9NN_m4i9Y_HRLp2OBxw");
